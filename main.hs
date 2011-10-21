099831dc708e460e889444c20f4a06d4a05d51f3
Grant Mathews <grant@causes.com>
2011-10-21 04:01:51 -0700

    It looks like node splits may be working.

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
-
 import Rect
 import Data.Maybe
 import Data.Ord
+import Debug.Trace
 import Data.Lens.Common
 import Control.Category
 import Prelude hiding ((.))
@@ -12,7 +12,14 @@ import Ptr
 data HTree = Node { _entries :: [HTree]
                   , _rect    :: Rect
                   , _lhv     :: Int
-                  } deriving (Show, Eq)
+                  } deriving (Eq)
+
+-- This is where you start to see Haskell's sex appeal
+showHTree indent (Node ch mbr lhv) = 
+  (concat $ replicate indent "  ") ++ "lhv: " ++ (show lhv) ++ " rect: " ++ (show mbr) ++ "\n" ++ (concat $ map (showHTree (indent + 2)) ch)
+
+instance Show HTree where
+  show n = showHTree 0 n
 
 -- lens for a single child of a Node
 childLens n = lens (\t -> (_entries t) !! n) -- get
@@ -46,16 +53,15 @@ updateList list val pos =
     (first, oldVal:rest) = splitAt pos list
 
 -- separate LIST into sublists of size SIZE.
--- TODO - instead of saying "i want grps of size 3" easier to say "i want 3 groups" also allows for sexy 3 `groupsOf` xx infixity
-groupsOf :: Int -> [a] -> [[a]]
-groupsOf size list = groupsOf' list size size [[]]
+groupsOf :: Show a => Int -> [a] -> [[a]]
+groupsOf size list = groupsOf' list size []
 
-groupsOf' :: [a] -> Int -> Int -> [[a]] -> [[a]]
-groupsOf' list cur size result
-  | null list = result
-  | cur == 0 = groupsOf'  (tail list) (size - 1) size (result ++ [[head list]])
-  | otherwise = groupsOf' (tail list) (cur - 1)  size ((init result) ++ [last result ++ [head list]])
-
+groupsOf' ::  Show a =>[a] -> Int -> [[a]] -> [[a]]
+groupsOf' list numGroups result
+  | numGroups == 0 = result
+  | otherwise = groupsOf' (drop takeAmt list) (numGroups - 1) (result ++ [take takeAmt list])
+  where
+    takeAmt = (length list) `div` numGroups
 
 --TODO
 hilbertValue :: Rect -> Int
@@ -107,37 +113,44 @@ insertRect root rect
 distributeEntriesToNodes :: [[HTree]] -> [Ptr HTree] -> HTree -> HTree
 
 distributeEntriesToNodes [] [] root = root
-distributeEntriesToNodes (grp:grps) (sib:sibs) root = 
-    distributeEntriesToNodes grps sibs newRoot
+distributeEntriesToNodes grps sibs root
+  | otherwise = distributeEntriesToNodes (tail grps) (tail sibs) newRoot
   where
-    newRoot = setNode sib root grp
+    newRoot = setNode (head sibs) root (head grps)
 
 handleOverflow :: Ptr HTree -> Rect -> HTree -> (Maybe HTree, HTree)
 handleOverflow node rect root =
     if allFull
       then let updatedRoot = distributeEntriesToNodes (tail entryGroups) (getSiblings node root) root
                --TODO actual bounding thing for group
-               splitNode   = Node (head entryGroups) rect (hilbertValue rect) in
+               splitNode   = Node (head entryGroups) (Rect 9 9 9 9) (hilbertValue rect) in
                  (Just splitNode, updatedRoot)
       else let updatedRoot = distributeEntriesToNodes entryGroups (getSiblings node root) root in
         (Nothing, updatedRoot)
   where
+    siblingList = (getSiblings node root)
     newEntry = Node [] rect (hilbertValue rect)
-    entries :: [HTree] = concat $ (map _entries $ (map (deref root) (getSiblings node root)))
-    entryGroups :: [[HTree]] = groupsOf 3 entries --TODO - not 3
+    entries :: [HTree] = concat $ (map _entries $ (map (deref root) siblingList))
     allFull = all full (map (deref root) (getSiblings node root))
+    entryGroups :: [[HTree]] = groupsOf ((length siblingList) + (if allFull then 1 else 0)) entries
 
 --TODO - i dont use siblings (var) here.
 
 --createdNode => split
 adjustTree :: Ptr HTree -> Maybe [Ptr HTree] -> Maybe HTree -> HTree -> HTree
-adjustTree updatedNode siblings createdNode root =
-    if isRoot updatedNode
-      then root
-      else let (pp, updatedRoot) = propNodeSplit
-               updatedRoot2 = updateMbrLhv updatedNode root in
-        
-        adjustTree (parent updatedNode) (Just $ getSiblings (parent updatedNode) updatedRoot2) pp updatedRoot2
+adjustTree updatedNode siblings createdNode root
+  | trace ("??" ++ show updatedNode) False = undefined
+  | otherwise = 
+      if isRoot updatedNode
+          then 
+            if isJust createdNode --split root! rejoin into one big root.
+              --todo-real values
+              then Node [root, fromJust createdNode] (Rect 0 0 100 100) 55
+              else root
+          else let (pp, updatedRoot) = propNodeSplit
+                   updatedRoot2 = updateMbrLhv updatedNode root in
+            
+            adjustTree (parent updatedNode) (Just $ getSiblings (parent updatedNode) updatedRoot2) pp updatedRoot2
 
   where
     -- better name might be maybePropNodeSplit...
@@ -146,10 +159,9 @@ adjustTree updatedNode siblings createdNode root =
       let np = parent updatedNode in
         case createdNode of
           Just newNode ->
-            let newNode = fromJust createdNode in    
-              if full $ newNode
-                then handleOverflow np (_rect newNode) root
-                else (Nothing, addToNode np root newNode)
+            if full $ newNode
+              then handleOverflow np (_rect newNode) root
+              else (Nothing, addToNode np root newNode)
           Nothing ->
             (Nothing, root)
 
@@ -167,13 +179,15 @@ adjustTree updatedNode siblings createdNode root =
 
 
 main = do
+    print $ 3 `groupsOf` [1, 2, 3]
+    print $ 3 `groupsOf` [1, 2, 3, 4]
+    print $ 3 `groupsOf` [1, 2, 3, 4, 5]
+    print $ 3 `groupsOf` [1, 2, 3, 4, 5, 6]
     print $ "Before"
     print $ tree
-    {-
     print $ "Add 1"
     print $ (insertRect tree (Rect 0 0 2 2))
     print $ "Add 2"
-    print $ insertRect (insertRect tree (Rect 0 0 2 99999992)) (Rect 0 0 5 5)
-    -}
+    print $ insertRect (insertRect tree (Rect 0 0 2 2)) (Rect 0 0 5 5)
   where
     tree = Node [Node [] (Rect 0 0 20 20) 20, Node [] (Rect 20 20 40 40) 45] (Rect 0 0 40 40) 200
